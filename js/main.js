29ed1b5eb34cd17e5875c03fa85ad64a7715816e
Anton Yushkov <a.s.yushkov@bk.ru>
2024-02-23 23:49:34 +0300

    4.16. Больше деталей

--- js/main.js
+++ js/main.js
@@ -1,35 +1,20 @@
-// id - число от 1 до 25. Идентификаторы не должны повторяться
-// url - строка — адрес картинки вида photos/{{i}}.jpg, где {{i}} — это число от 1 до 25. Адреса картинок не должны повторяться
-// description -  описание фотографии ???
-// likes - cлучайное число от 15 до 200
-// comments - массив объектов — список комментариев
-//        оставленных другими пользователями к этой фотографии
-//        количество комментариев к каждой фотографии — случайное число от 0 до 30
-//        все комментарии генерируются случайным образом
-// {
-//   id: 135, // id — любое число - идентификаторы не должны повторяться
-//   avatar: 'img/avatar-6.svg',
-//        avatar — это строка, значение которой формируется по правилу img/avatar-{{случайное число от 1 до 6}}.svg.
-//        аватарки подготовлены в директории img
-//   message: 'В целом всё неплохо. Но не всё.',
-//   name: 'Артём', // Имена авторов также должны быть случайными. Набор имён для комментаторов составьте сами.
-// }
+const PHOTO_ID_COUNT_MAX = 25; // необходимое кол-во объектов
+const PHOTO_PATH = 'photos/'; // адрес папки с фото
+const PHOTO_LIKES_MIN = 15; // кол-во лайков
+const PHOTO_LIKES_MAX = 200;
+const PHOTO_DESCRIPTION_MIN_LENGTH = 20; // длинна случайного описания фото
+const PHOTO_DESCRIPTION_MAX_LENGTH = 100;
+const COMMENTS_AVATAR_PATH = 'img/'; // адрес папки с аватарками
+const AVATAR_COUNTS_MIN = 1; // кол-во аватарок
+const AVATAR_COUNTS_MAX = 6;
+const COMMENTS_ID_LENGTH = 6; // длинна случайного идентификатора комментария
+const COMMENTS_COUNT_MIN = 0; // кол-во комментариев к объекту
+const COMMENTS_COUNT_MAX = 30;
+const COMMENT_MESSEGES_COUNT_MAX = 2; // кол-во случайных строк из которых собираются комментарии
+const COMMENTS_IDS = new Set(); // тут хранятся ID комментариев для проверки их на уникальность
+const PHOTO_DATA = []; // сюда записывается результат всех операций
 
-const photoData = {
-  id: 1,
-  url: 'photos/1.jpg',
-  description: 'рандом',
-  likes: 200,
-  comments: {
-    id: 135,
-    avatar: 'img/avatar-6.svg',
-    message: 'В целом всё неплохо. Но не всё.',
-    name: 'Артём'
-  }
-};
-
-
-const messeges = [
+const USER_MESSEGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра.',
@@ -40,7 +25,7 @@ const messeges = [
   'Как можно было поймать такой неудачный момент?!'
 ];
 
-const userNames = [
+const USER_NAMES = [
   'Мия','Анастасия','Михаил','Артур','Григорий','Софья','Ульяна',
   'Есения','Андрей','Алексей','София','Лев','Полина','Аиша','Иван',
   'Наталья','Фёдор','Никита','Артём','Василиса','Александр','Алина',
@@ -48,16 +33,19 @@ const userNames = [
   'Даниил','Кирилл','Максим','Евгений','Тимур','Мария','Вероника',
   'Кристина','Марта','Елизавета','Стефания','Виктория','Диана','Николь',
   'Дарья','Гордей','Ольга','Варвара','Матвей','Элина','Карим','Валерия'
-]
+];
 
-function getRandomNumber (a, b) {
-  const lower = Math.ceil(Math.min(a, b));
-  const upper = Math.floor(Math.max(a, b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
+
+function getRandomNumber (from, before) {
+  const LOWER = Math.ceil(Math.min(from, before));
+  const UPPER = Math.floor(Math.max(from, before));
+  const RESULT = Math.random() * (UPPER - LOWER + 1) + LOWER;
+  return Math.floor(RESULT);
 }
+// console.log(getRandomNumber(5, 10));
 
-function createIdGenerator () {
+
+function getNewID () {
   let lastGeneratedId = 0;
 
   return function () {
@@ -66,8 +54,127 @@ function createIdGenerator () {
   };
 }
 
-function getPhotoID () {
-  return getRandomNumber(1, 25);
+
+const getPhotoID = getNewID();
+const getPhotoPathID = getNewID();
+
+
+function getPhotoURL (path) {
+  const URL = `${path + getPhotoPathID()}.jpg`;
+  return URL;
 }
 
-console.log(getPhotoID);
+
+function getRandomString (desiredStringLength = 1) {
+  const PRIMER = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+  for (let i = 0; i < desiredStringLength; i++) {
+    randomString += PRIMER.charAt(Math.floor(Math.random() * PRIMER.length));
+  }
+  return randomString;
+}
+// console.log(getRandomString(5));
+
+
+function getDescription (){
+  return getRandomString(getRandomNumber(PHOTO_DESCRIPTION_MIN_LENGTH, PHOTO_DESCRIPTION_MAX_LENGTH));
+}
+// console.log(getDescription());
+
+
+function getRandomUserName () {
+  const RANDOM_NAME_INDEX = getRandomNumber(0, USER_NAMES.length - 1);
+  return USER_NAMES[RANDOM_NAME_INDEX];
+}
+// console.log(getRandomUserName());
+
+
+function getRandomLikesCount () {
+  return getRandomNumber(PHOTO_LIKES_MIN, PHOTO_LIKES_MAX);
+}
+// console.log(getRandomLikesCount());
+
+
+function getAvatarImgURL () {
+  return `${COMMENTS_AVATAR_PATH}avatar-${getRandomNumber(AVATAR_COUNTS_MIN, AVATAR_COUNTS_MAX)}`;
+}
+// console.log(getAvatarImgURL());
+
+
+function getID(length) {
+  let id = '';
+  for (let i = 0; i < length; i++) {
+    id += getRandomNumber(1,9);
+  }
+  return id;
+}
+// console.log(getID(COMMENTS_ID_LENGTH));
+
+
+function getUniqueCommentID (){
+  const id = getID(COMMENTS_ID_LENGTH);
+  while (!COMMENTS_IDS.has(id)) {
+    COMMENTS_IDS.add(id);
+    break;
+  }
+  return id;
+}
+// console.log(getUniqueCommentID());
+// console.log(COMMENTS_IDS);
+
+
+function getCommentMessage (count){
+  const COUNT_MESEEGES = getRandomNumber(1, count);
+  let message = '';
+  for (let i = 0; i < COUNT_MESEEGES; i++){
+    message += (` ${ USER_MESSEGES[getRandomNumber(0, USER_MESSEGES.length - 1)]}`);
+  }
+  return message;
+}
+// console.log(getCommentMessage(COMMENT_MESSEGES_COUNT_MAX));
+
+
+function getComment (){
+  return {
+    id: getUniqueCommentID(),
+    avatar: getAvatarImgURL(),
+    message: getCommentMessage(COMMENT_MESSEGES_COUNT_MAX),
+    name: getRandomUserName()
+  };
+}
+// console.log(getComment());
+
+
+function getCommentsArray (){
+  const COUNT_COMMENTS = getRandomNumber(COMMENTS_COUNT_MIN, COMMENTS_COUNT_MAX);
+  // console.log(COUNT_COMMENTS);
+  const COMMENTS = [];
+  for (let i = 0; i < COUNT_COMMENTS; i++){
+    const NEW_COMMENT = getComment();
+    COMMENTS.push(NEW_COMMENT);
+  }
+  return COMMENTS;
+}
+// console.log(getCommentsArray());
+
+
+function getOnePhotoData (){
+  return {
+    id: getPhotoID(),
+    url: getPhotoURL(PHOTO_PATH),
+    description: getDescription(),
+    likes: getRandomLikesCount(),
+    comments: getCommentsArray()
+  };
+}
+// console.log(getOnePhotoData());
+
+
+function getPhotoData (){
+  for (let i = 0; i < PHOTO_ID_COUNT_MAX; i++) {
+    const NEW_PHOTO_DATA = getOnePhotoData();
+    PHOTO_DATA.push(NEW_PHOTO_DATA);
+  }
+  return PHOTO_DATA;
+}
+console.table(getPhotoData()); // результат всех операций
